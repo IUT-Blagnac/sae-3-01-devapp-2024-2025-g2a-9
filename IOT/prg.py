9933af2f67071d533042c8a425a0ff4e439f0660
Perceval00731 <fbthomas9@gmail.com>
2024-11-06 11:36:34 +0100

    fin tp1 iot

--- IOT/prg.py
+++ IOT/prg.py
@@ -3,33 +3,42 @@
 import paho.mqtt.client as mqtt
 import json
 import logging
+import configparser
+import os
 
-# Configuration
-mqttServer = "mqtt.iut-blagnac.fr"
-topics = ["AM107/#", "solaredge/#"]
+#on lit le fichier de config
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+#on utilisera une valeur par defaut pour le serveur et les topics si aucune valeur n'est fournie dans le fichier de config
+mqttServer = config.get('MQTT', 'server', fallback='mqtt.iut-blagnac.fr')
+topics = config.get('MQTT', 'topics', fallback='AM107/#, solaredge/#').split(',')
 
 logging.basicConfig(level=logging.INFO)
+
+
+file_path = 'donnees.txt'
+file_descriptor = os.open(file_path, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
 
 
 def on_message(client, userdata, msg):
     print(f"Message reçu sur le topic {msg.topic}: OK")
     try:
-        #deserialisation
+        #on deserialise le message
         payload_str = msg.payload.decode()
         jsonMsg = json.loads(payload_str)
         
         if "AM107" in msg.topic:
             if isinstance(jsonMsg, list) and len(jsonMsg) >= 2:
-                
                 sensor_data = jsonMsg[0]
-                
                 device_info = jsonMsg[1]
                 
                 temperature = sensor_data.get('temperature')
                 room = device_info.get('room')
                 
                 if temperature is not None and room is not None:
-                    print(f"Température: {temperature} °C dans la salle {room}")
+                    data_str = f"Température: {temperature} °C dans la salle {room}\n"
+                    os.write(file_descriptor, data_str.encode())
                 else:
                     logging.error("Température ou salle non trouvée dans les données")
             else:
@@ -39,14 +48,15 @@ def on_message(client, userdata, msg):
             lastDayData = jsonMsg.get('lastDayData', {}).get('energy')
             
             if lastDayData is not None:
-                print(f"Production solaire sur la dernière journée: {lastDayData} Wh")
+                data_str = f"Production solaire sur la dernière journée: {lastDayData} Wh\n"
+                os.write(file_descriptor, data_str.encode())
             else:
                 logging.error("Données de production solaire non trouvées")
     
     except json.JSONDecodeError as e:
         logging.error("Erreur de décodage JSON : %s", e)
 
-#connexion au broker
+#connexion et souscription
 client = mqtt.Client()
 client.on_message = on_message
 client.connect(mqttServer, port=1883, keepalive=60)
@@ -56,3 +66,6 @@ for topic in topics:
     client.subscribe(topic.strip(), qos=0)
 
 client.loop_forever()
+
+#fermeture du fichier
+os.close(file_descriptor)
