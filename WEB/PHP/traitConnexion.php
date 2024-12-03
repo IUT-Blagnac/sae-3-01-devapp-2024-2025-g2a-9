714d78120db826877eb79854e22597907218d6f8
GUILLUY Matt <148845318+MASTTTTT@users.noreply.github.com>
2024-12-03 17:51:03 +0100

    test mdp dure

--- WEB/PHP/traitConnexion.php
+++ WEB/PHP/traitConnexion.php
@@ -13,32 +13,36 @@
         }
 
         // Vérifie si les crédentials existents
-        $req = $conn->prepare("SELECT nom, prenom FROM CLIENT
-                                    WHERE mail = ? AND password = ?") ;
-        $req->execute([$email, password_hash($pwd, PASSWORD_DEFAULT)]);
+        $req = $conn->prepare("SELECT nom, prenom, password FROM CLIENT
+                                    WHERE mail = ?") ;
+        $req->execute([$email]);
 
         if ($client = $req->fetch()) {
-            $nom = $result['nom'];
-            $prenom = $result['prenom'];
-            $_SESSION['user'] = $email;
+            if ($pwd === $client['password']) {
+                $nom = $client['nom'];
+                $prenom = $client['prenom'];
+                $_SESSION['user'] = $email;
 
-            if($cookie) {
-            setcookie('user_email', $email, time()+60*10);
-            }
-            // Renvoi sur la page en cours (could)
-            if (isset($_SESSION['url'])) {
-                $url = $_SESSION['url'];
-                unset($_SESSION['url']);
+                if($cookie) {
+                setcookie('user_email', $email, time()+60*10);
+                }
+                // Renvoi sur la page en cours (could)
+                // if (isset($_SESSION['url'])) {
+                //     $url = $_SESSION['url'];
+                //     unset($_SESSION['url']);
+                //     header("location:index.php");
+                //     exit();
+                // }
                 header("location:index.php");
                 exit();
             }
             else {
-                header("location:index.php");
+                header("location: formConnexion.php?msgErreur=Le mot de passe est incorrect.");
                 exit();
             }
         }
         else {
-            header("location: formConnexion.php?msgErreur=L'adresse E-mail ou mot de passe est incorrect.");
+            header("location: formConnexion.php?msgErreur=L'adresse e-mail est incorrect.");
             exit();
         }
     }
