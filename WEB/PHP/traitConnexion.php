ffb27534f2c771f7a208afed05b644d22d81894b
GUILLUY Matt <148845318+MASTTTTT@users.noreply.github.com>
2024-12-03 15:35:16 +0100

    conn BD + traitConnexion

---
+++ WEB/PHP/traitConnexion.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+    require_once "./include/connect.inc.php";
+
+    if (isset($_POST['submit'])) {
+        // Récupère données form
+        $email = htmlentities($_POST['mail']);
+        $pwd = htmlentities($_POST['pwd']);
+        $cookie = htmlentities($_POST['cookie']);
+        // Vérifie si les crédentials existents
+        $req = $conn->prepare("SELECT nom, prenom FROM Client
+                                    WHERE mail = ? AND password = ?") ;
+        $req->execute([$email, password_hash($pwd)]);
+        if ($client = $req->fetch();) {
+            $nom = $result['nom'];
+            $prenom = $result['prenom'];
+            $_SESSION['user'] = $email;
+            if($cookie) {
+            setcookie('C'.$nom.$prenom, $email, time()+60*10);
+            }
+            // Renvoi sur la page en cours
+            if (isset($_SESSION['url'])) {
+                $url = $_SESSION['url'];
+                unset($_SESSION['url']);
+                header("location:$url");
+                exit();
+            }
+            else {
+                header("location:index.php");
+                exit();
+            }
+        }
+    }
+?>
