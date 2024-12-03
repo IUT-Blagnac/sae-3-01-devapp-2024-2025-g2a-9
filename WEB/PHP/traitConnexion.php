c2f384465d2cecb69199c6f6558b96ca5f66bad3
GUILLUY Matt <148845318+MASTTTTT@users.noreply.github.com>
2024-12-03 16:42:25 +0100

    BD CLIENT != Client

--- WEB/PHP/traitConnexion.php
+++ WEB/PHP/traitConnexion.php
@@ -9,20 +9,17 @@
         $cookie = htmlentities($_POST['cookie']);
 
         // Vérifie si les crédentials existents
-        $req = $conn->prepare("SELECT nom, prenom FROM Client
+        $req = $conn->prepare("SELECT nom, prenom FROM CLIENT
                                     WHERE mail = ? AND password = ?") ;
         $req->execute([$email, password_hash($pwd, PASSWORD_DEFAULT)]);
 
-        $testEmail = "test@outlook.fr";
-        $testPwd = "test";
-
-        if ($client = $req->fetch() || ($testEmail === $email && $testPwd === $pwd)) {
+        if ($client = $req->fetch()) {
             $nom = $result['nom'];
             $prenom = $result['prenom'];
             $_SESSION['user'] = $email;
 
             if($cookie) {
-            setcookie('C'.$nom.$prenom, $email, time()+60*10);
+            setcookie('user_email', $email, time()+60*10);
             }
             // Renvoi sur la page en cours
             if (isset($_SESSION['url'])) {
