858d1073a69402eedc523fefdb35b67f2b188bac
Perceval <fbthomas9@gmail.com>
2024-12-08 22:52:24 +0100

    ajout instructions lancement sur linux

--- IOT/Application/src/main/java/application/NauticGestApp.java
+++ IOT/Application/src/main/java/application/NauticGestApp.java
@@ -1,5 +1,12 @@
 package application;
 
+
+// sur linux lancer depuis le jar depuis le repertoire Application avec
+// java --module-path target/dependencies --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar target/NauticGest-1.0.jar
+// attention de bien avoir javafx-graphics-17-linux.jar sinon le prendre ici : https://repo1.maven.org/maven2/org/openjfx/javafx-graphics/17/
+
+
+
 import application.control.MenuController;
 import javafx.application.Application;
 import javafx.stage.Stage;
