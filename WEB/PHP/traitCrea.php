45a1b6d562b843ec6c2fa8e7f31f515c4a7190fa
obsqha <161354946+obsqha@users.noreply.github.com>
2024-12-08 14:56:31 +0100

    version final creation de compte
    version final du traitement et du fromulaire

--- WEB/PHP/traitCrea.php
+++ WEB/PHP/traitCrea.php
@@ -11,13 +11,25 @@
         $civilite = htmlentities($_POST['civilite']);
         $Numtel = htmlentities($_POST['Numtel']);
         $verif = htmlentities($_POST['verif']);
+        $pays = htmlentities($_POST['pays']);
 
         if($mdp!=$verif){
-            header("location: FormCrea.php?msgErreur=Mot de passe saisie différent lors de la vérification");
+            header("location: formCrea.php?msgErreur=Mot de passe saisie différent lors de la vérification");
             exit();
         }
+        $reqmail = $conn->prepare("SELECT * FROM UTILISATEUR
+                                   WHERE mail = ?") ;
+        $reqmail->execute([$mail]);
 
-        $req = $conn->prepare("INSERT INTO Client (nom,prenom,civilite,dateN,mail,telephone,password)
-                               VALUES (?,?,?,?,?,?,?)") ;
-        $req->execute([$nom,$prenom,$civilite,$dateN,$mail,$Numtel,$mdp]);
+        if ($user = $reqmail->fetch()) {
+            header("location: formCrea.php?msgErreur=Compte déja existant pour cette adresse");
+            exit();
+        }else{
+            $password = password_hash($mdp,PASSWORD_DEFAULT);
+            $req = $conn->prepare("INSERT INTO UTILISATEUR (nom,prenom,civilite,dateN,mail,telephone,password,pays,droit)
+                                   VALUES (?,?,?,?,?,?,?,?,'CLIENT')") ;
+            $req->execute([$nom,$prenom,$civilite,$dateN,$mail,$Numtel,$password,$pays]);
+            header("location:formConnexion.php");
+            exit();
+        }
     }
