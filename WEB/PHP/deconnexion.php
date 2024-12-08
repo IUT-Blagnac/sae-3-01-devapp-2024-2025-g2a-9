c16ac488bf1e950005618a7aa049c614d14c2cc8
MASTTTTT <guilluy.matt0@gmail.com>
2024-12-08 16:52:04 +0100

    maj deconnexion

--- WEB/PHP/deconnexion.php
+++ WEB/PHP/deconnexion.php
@@ -1,5 +1,10 @@
 <?php
+    session_start();
     if (isset($_SESSION['user'])) { 
         unset($_SESSION['user']);
+        session_destroy();
+
+        header("location:index.php");
+        exit;
     }
 ?>
